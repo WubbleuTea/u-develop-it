e6a7808a5248b2bc8b47ffaec52a49a70ffafb0c
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-08-02 16:53:15 -0500

    hard coded database requests

--- server.js
+++ server.js
@@ -1,17 +1,58 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const sqlite3 = require('sqlite3').verbose();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = new sqlite3.Database('./db/elections.db', err => {
+    if (err) {
+        return console.error(err.message)
+    }
+
+    console.log('Connected to the election database.')
+})
+// Gets all candidates from db
+// db.all('SELECT * FROM candidates', (err, rows) => {
+//     console.log(rows)
+// });
+
+// //GET a single candidate
+// db.get('SELECT * FROM candidates WHERE id = 1', (err, row) => {
+//     if(err) {
+//         console.log(err)
+//     }
+//     console.log(row)
+// })
+
+// // Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// })
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID)
+});
 // Default response for any other requests(Not Found) Catch All
 app.use((req, res) => {
     res.status(404).end();
 });
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
