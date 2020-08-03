71062abec0a8a9a3a7a5be8c8415fab081c1a978
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-08-02 23:56:05 -0500

    updated server order

--- server.js
+++ server.js
@@ -60,6 +60,78 @@ app.get('/api/candidate/:id', (req, res) => {
     });
 });
 
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                    VALUES (?,?,?)`
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).jason({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
+
+// Update party_id
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    
+    const sql = `UPDATE candidates SET party_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+  
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: req.body,
+            changes: this.changes
+        });
+    });
+});
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE ID = ?`;
+    const params = [req.params.id];
+    db.run (sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return
+        }
+
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+
+
 // GET all parties
 app.get('/api/parties', (req, res) => {
     const sql = `SELECT * FROM parties`;
@@ -93,48 +165,6 @@ app.get('/api/party/:id', (req, res) => {
     });
 });
 
-// // Delete a candidate
-app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE ID = ?`;
-    const params = [req.params.id];
-    db.run (sql, params, function(err, result) {
-        if (err) {
-            res.status(400).json({ error: res.message });
-            return
-        }
-
-        res.json({
-            message: 'successfully deleted',
-            changes: this.changes
-        });
-    });
-});
-
-app.put('/api/candidate/:id', (req, res) => {
-    const errors = inputCheck(req.body, 'party_id');
-
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-    
-    const sql = `UPDATE candidates SET party_id = ? 
-                 WHERE id = ?`;
-    const params = [req.body.party_id, req.params.id];
-  
-    db.run(sql, params, function(err, result) {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: req.body,
-            changes: this.changes
-        });
-    });
-});
 
 app.delete('/api/party/:id', (req, res) => {
     const sql = `DELETE FROM parties WHERE id = ?`;
@@ -149,31 +179,7 @@ app.delete('/api/party/:id', (req, res) => {
     });
 });
 
-// Create a candidate
-app.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
 
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-                    VALUES (?,?,?)`
-    const params = [body.first_name, body.last_name, body.industry_connected];
-    // ES5 function, not arrow function, to use `this`
-    db.run(sql, params, function(err, result) {
-        if (err) {
-            res.status(400).jason({ error: err.message });
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: body,
-            id: this.lastID
-        });
-    });
-});
 
 app.use((req, res) => {
     res.status(404).end();
