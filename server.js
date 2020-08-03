b7aeb6c03b7233d4f2715f978f9229eecbe9078d
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-08-02 23:41:30 -0500

    added table joining functionality

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ const db = new sqlite3.Database('./db/elections.db', err => {
     console.log('Connected to the election database.')
 })
 // Gets all candidates from db
-app.get('/api/candidate', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name
+                    AS party_name
+                    FROM candidates
+                    LEFT JOIN parties
+                    ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
@@ -36,7 +40,12 @@ app.get('/api/candidate', (req, res) => {
 
 // //GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                    AS party_name
+                    FROM candidates
+                    LEFT JOIN parties
+                    ON candidates.party_id = parties.i
+                    WHERE id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
